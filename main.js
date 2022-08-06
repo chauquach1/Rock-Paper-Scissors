1e7ec72fd90031f4a2b0d0402fea0c0b4a0f5af2
chauquach1 <110010009+chauquach1@users.noreply.github.com>
2022-08-06 13:55:58 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+var getUserChoice = function (userInput) {
+    userInput = UserInput.toLowerCase();
+    if (userInput==='rock'||userInput==='paper'||userInput==='paper') {
+        return userInput
+    } else console.log('Try Again');
+}
+// When invoking getUserName('ROCK'), function returns 'rock'. Is my function returning rock or 'rock'?
 
+var getComputerChoice = function () {
+    Math.floor(Math.random()*2)
+    if (getComputerChoice === 0) {
+        return 0;
+    } else if (getComputerChoice === 1) {
+        return 1; 
+    } else {
+        return 2;
+    }
+    return getComputerChoice;
+}
+
+var determineWinner = function(userChoice , computerChoice){
+
+}
