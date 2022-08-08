e403fc12fbbaf92e09e4a18c373c2638d74a2838
chauquach1 <110010009+chauquach1@users.noreply.github.com>
2022-08-07 21:20:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
-var getUserChoice = function (userInput) {
-    userInput = UserInput.toLowerCase();
-    if (userInput==='rock'||userInput==='paper'||userInput==='paper') {
+var getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+    if (userInput==='rock'||userInput==='paper'||userInput==='scissors'||userInput==='bomb') {
         return userInput
     } else console.log('Try Again');
 }
-// When invoking getUserName('ROCK'), function returns 'rock'. Is my function returning rock or 'rock'?
 
-var getComputerChoice = function () {
-    Math.floor(Math.random()*2)
-    if (getComputerChoice === 0) {
-        return 0;
-    } else if (getComputerChoice === 1) {
-        return 1; 
+var getComputerChoice = (computerChoice) => {
+    computerChoice = Math.floor(Math.random()*3)
+    if (computerChoice === 0) {
+        return 'rock';
+    } else if (computerChoice === 1) {
+        return 'paper'; 
     } else {
-        return 2;
+        return 'scissors';
     }
-    return getComputerChoice;
-}
+ } 
 
-var determineWinner = function(userChoice , computerChoice){
+var determineWinner = (userChoice, getComputerChoice) => {
+    if (userChoice === 'bomb') {
+        return 'You Won'
+    } else if (userChoice === getComputerChoice) {
+        return 'The Game Was Tied';
+    } else if (userChoice === 'rock' && getComputerChoice === 'paper') {
+        return 'You Lost';
+    } else if (userChoice === 'rock' && getComputerChoice === 'scissors') {
+        return 'You Won';
+    } else if (userChoice === 'paper' && getComputerChoice === 'scissors') {
+        return 'You Lost';
+    } else if (userChoice === 'paper' && getComputerChoice === 'rock') {
+        return 'You Won';
+    } else if (userChoice === 'scissors' && getComputerChoice === 'rock') {
+        return 'You Lost';
+    } else if (userChoice === 'scissors' && getComputerChoice === 'paper') {
+        return 'You Won';
+    } 
+} 
 
-}
+var playGame = () => {
+    var userChoice = getUserChoice('bomb');
+    console.log(userChoice);
+    var computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    return determineWinner(userChoice, computerChoice);
+} 
+console.log(playGame());
